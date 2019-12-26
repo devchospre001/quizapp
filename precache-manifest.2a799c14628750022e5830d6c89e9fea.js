22708f8e903dae7b0d9a6f44f351636c56047837
Abdul-Kerim Spreco <sprecoabdulkerim@gmail.com>
2019-12-26 01:36:42 +0100

    Updates

---
+++ precache-manifest.2a799c14628750022e5830d6c89e9fea.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb1ff3e980a7c61ecae9a8685b64bb8",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "a79d7386ed561441d23c",
+    "url": "/quizapp/static/css/2.03b28e72.chunk.css"
+  },
+  {
+    "revision": "f231246e0d805ad183da",
+    "url": "/quizapp/static/css/main.ed0c20bd.chunk.css"
+  },
+  {
+    "revision": "a79d7386ed561441d23c",
+    "url": "/quizapp/static/js/2.311022a4.chunk.js"
+  },
+  {
+    "revision": "2ffdd1580e18724eb3fbd3655c041c87",
+    "url": "/quizapp/static/js/2.311022a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f231246e0d805ad183da",
+    "url": "/quizapp/static/js/main.07bc4c23.chunk.js"
+  },
+  {
+    "revision": "8bef646db43f26647d42",
+    "url": "/quizapp/static/js/runtime-main.e932e1f2.js"
+  },
+  {
+    "revision": "f05e37d5dde17938e5efbd6ff5876374",
+    "url": "/quizapp/static/media/answer.f05e37d5.png"
+  },
+  {
+    "revision": "c0302796e984ecbb92c4b065a1c1a92f",
+    "url": "/quizapp/static/media/background.c0302796.jpg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/quizapp/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/quizapp/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "f06f3bfe9454949bce2baa2f2af4d2ec",
+    "url": "/quizapp/static/media/fiftyFifty.f06f3bfe.PNG"
+  },
+  {
+    "revision": "145bf2178ec018524dadd29e831cc8a3",
+    "url": "/quizapp/static/media/hints.145bf217.PNG"
+  },
+  {
+    "revision": "1618c77b6c5c11926819a8868a083031",
+    "url": "/quizapp/static/media/materialdesignicons-webfont.1618c77b.ttf"
+  },
+  {
+    "revision": "927457ed7478ec7e1580a2b88116f9c3",
+    "url": "/quizapp/static/media/materialdesignicons-webfont.927457ed.woff2"
+  },
+  {
+    "revision": "d6e3eba9b16453f51d4ef6e216bf89ff",
+    "url": "/quizapp/static/media/materialdesignicons-webfont.d6e3eba9.woff"
+  },
+  {
+    "revision": "f81583fc4d8aa5aa2820976561ea8aec",
+    "url": "/quizapp/static/media/materialdesignicons-webfont.f81583fc.eot"
+  },
+  {
+    "revision": "0ab14d143f1993fd0a065be0e0aa58a5",
+    "url": "/quizapp/static/media/options.0ab14d14.PNG"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/quizapp/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
